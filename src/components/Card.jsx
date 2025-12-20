e841c8890aecdd9fd2d279991a372b54a6c12f96
devwithsk <devwithsk@gmail.com>
2025-12-20 12:37:35 +0530

    added functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { EllipsisVertical } from 'lucide-react'
 
 const Card = () => {
+  const [parsedData, setParsedData] = useState([])
+
+  useEffect(() => {
+    const fetchTasks = () => {
+      let data = localStorage.getItem("tasks")
+      if(data){
+        setParsedData(JSON.parse(data).reverse())
+      }
+    }
+    fetchTasks()
+
+    window.addEventListener('taskUpdated', fetchTasks)
+    return () => window.removeEventListener('taskUpdated', fetchTasks)
+  }, [])
+  
+
+  
+
   return (
     <>
-        <div className='h-fit border border-white rounded-xl bg-gray-800 shadow-lg flex-col'>
+        {parsedData && parsedData.map((item, index) => (
+          <div key={index} className='h-fit border border-white rounded-xl bg-gray-800 shadow-lg flex-col break-inside-avoid mb-4'>
             <div className='w-full h-1/2 border-b border-gray-700 py-1 px-2 flex justify-between items-center'>
-                <h3 className='text-left text-white font-medium text-md md:text-xl'>Aj ka gyan smapt</h3>
+                <h3 className='text-left text-white font-medium text-md md:text-xl'>{item.title ? item.title : "Untitled"}</h3>
                 <EllipsisVertical className="cursor-pointer" />
             </div>
 
             <div className='h-1/2 w-full'>
-                <p className='text-white p-4 text-sm md:text-lg text-left'>This is a sample task description. It gives more details about the task to be done This is a sample task description. It gives more details about the task to be done.</p>
+                <p className='text-white p-4 text-sm md:text-lg text-left whitespace-pre-wrap'>{item.task}</p>
             </div>
         </div>
+        ))}
     </>
 
     
--- src/components/Viewtask.jsx
+++ src/components/Viewtask.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Card from './Card'
 
+
 const Viewtask = ({ className }) => {
   return (
-    <div className={`text-white w-full h-full overflow-y-scroll grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4 ${className}`}>
+    <div className={`text-white w-full h-full overflow-y-scroll columns-1 md:columns-2 lg:columns-3 xl:columns-4 gap-6 p-4 ${className}`}>
         <Card   />
     </div>
   )
